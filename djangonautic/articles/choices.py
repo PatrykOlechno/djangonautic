29e13040caa7dc91bf5fd730aaa1b692c8afe0a6
Patryk Olechno <patrykoolechno@gmail.com>
2018-06-18 17:18:09 +0200

    added feelings stamp

---
+++ djangonautic/articles/choices.py
@@ -0,0 +1,6 @@
+FEEL_CHOICE = (
+    (1, "[Super!]"),
+    (2, "[Nice!]"),
+    (3, "[Sad :(]"),
+    (4, "[Soo Bad :<]")
+)
--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from articles.choices import *
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
+    feel = models.IntegerField(choices=FEEL_CHOICE, default=1)
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
